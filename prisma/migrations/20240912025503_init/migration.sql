f6a653cef49a6234e72e3c725794729eb05021ca
José Marcolino <josetyu52@gmail.com>
2024-10-09 23:50:12 -0300

    feat: rotas get, put e delete

--- prisma/migrations/20240912025503_init/migration.sql
+++ prisma/migrations/20240912025503_init/migration.sql
@@ -7,6 +7,7 @@ CREATE TABLE "Usuario" (
     "data_nascimento" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT TRUE,
 
     CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
 );
--- src/controller/usuarios.js
+++ src/controller/usuarios.js
@@ -17,4 +17,60 @@ cadastrarUsuario.post("/usuarios", async (req, res) => {
   }
 });
 
+
+cadastrarUsuario.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await prisma.usuario.findMany({
+      where: {
+        deletedAt: null, 
+      },
+    });
+    res.status(200).json(usuarios);
+  } catch (error) {
+    
+    res.status(500).json({ error: "Erro ao listar usuários." });
+  }
+});
+
+cadastrarUsuario.put("/usuarios/:id", async (req, res) => {
+  const { id } = req.params; 
+  const { nome, email, senha, data_nascimento } = req.body; 
+  try {
+    const usuarioAtualizado = await prisma.usuario.update({
+      where: { id: Number(id) }, 
+      data: {
+        nome,
+        email,
+        senha,
+        data_nascimento: new Date(data_nascimento),
+      },
+    });
+    res.status(200).json(usuarioAtualizado);
+  } catch (error) {
+    if (error.code === 'P2025') {
+      return res.status(404).json({ error: "Usuário não encontrado." });
+    }
+    res.status(500).json({ error: "Erro ao atualizar usuário." });
+  }
+});
+
+cadastrarUsuario.delete("/usuarios/:id", async (req, res) => {
+  const { id } = req.params; 
+
+  try {
+    const usuarioExcluido = await prisma.usuario.update({
+      where: { id: Number(id) },
+      data: { deletedAt: new Date() }, 
+    });
+
+    res.status(200).json({ mensagem: "Usuário excluído logicamente.", usuario: usuarioExcluido });
+  } catch (error) {
+    if (error.code === 'P2025') {
+      return res.status(404).json({ error: "Usuário não encontrado." });
+    }
+    res.status(500).json({ error: "Erro ao excluir usuário." });
+  }
+});
+
+
 module.exports = {cadastrarUsuario};
